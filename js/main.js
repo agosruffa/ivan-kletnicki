d67e9701e0d4effc76aef0be725e552c5b472c6f
agosruffa <69657713+agosruffa@users.noreply.github.com>
2021-01-29 17:20:14 -0300

    portfolio final + menu hamburger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 //menu 
-
+$(document).ready(function(){
+  $("a").click(function(){
+    $("#collapsibleNavbar").hide();
+  });
+  $("button").click(function(){
+    $("#collapsibleNavbar").show();
+  });
+});
 
 // portfolio 
 
@@ -226,6 +233,129 @@ function showDivs6(n) {
   var x = document.getElementsByClassName("mySlides6");
   var dots = document.getElementsByClassName("demo6");
   var captionText = document.getElementById("caption6");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+// septima linea 
+
+function openModal7() {
+  document.getElementById('myModal7').style.display = "block";
+}
+
+function closeModal7() {
+  document.getElementById('myModal7').style.display = "none";
+}
+
+var slideIndex = 1;
+showDivs7(slideIndex);
+
+function plusDivs7(n) {
+  showDivs7(slideIndex += n);
+}
+
+function currentDiv7(n) {
+  showDivs7(slideIndex = n);
+}
+
+function showDivs7(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides7");
+  var dots = document.getElementsByClassName("demo7");
+  var captionText = document.getElementById("caption7");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
+
+// octava linea 
+
+function openModal8() {
+  document.getElementById('myModal8').style.display = "block";
+}
+
+function closeModal8() {
+  document.getElementById('myModal8').style.display = "none";
+}
+
+var slideIndex = 1;
+showDivs8(slideIndex);
+
+function plusDivs8(n) {
+  showDivs8(slideIndex += n);
+}
+
+function currentDiv8(n) {
+  showDivs8(slideIndex = n);
+}
+
+function showDivs8(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides8");
+  var dots = document.getElementsByClassName("demo8");
+  var captionText = document.getElementById("caption8");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+// novena linea 
+
+function openModal9() {
+  document.getElementById('myModal9').style.display = "block";
+}
+
+function closeModal9() {
+  document.getElementById('myModal9').style.display = "none";
+}
+
+var slideIndex = 1;
+showDivs9(slideIndex);
+
+function plusDivs9(n) {
+  showDivs9(slideIndex += n);
+}
+
+function currentDiv9(n) {
+  showDivs9(slideIndex = n);
+}
+
+function showDivs9(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides9");
+  var dots = document.getElementsByClassName("demo9");
+  var captionText = document.getElementById("caption9");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
